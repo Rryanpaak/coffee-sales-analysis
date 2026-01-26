b208c68a97dbd69534b507760051723da6ebce93
Rryanpaak <park784612@gmail.com>
2026-01-26 18:05:12 +0900

    Update 01_validation.sql

--- sql/01_validation.sql
+++ sql/01_validation.sql
@@ -3,6 +3,17 @@
 
 -- 1) Row_count
 select count(*) AS row_count
-from coffee_sales
+from coffee_sales;
 
 -- 2) Date range
+SELECT 
+	min(date) as start_date,
+	max(date) as end_date
+FROM coffee_sales;
+
+-- 3) Checking Null
+SELECT
+	SUM(CASE WHEN date IS NULL OR TRIM(date) = '' THEN 1 ELSE 0 END) AS null_date,
+	SUM(CASE WHEN money IS NULL OR TRIM(money) = '' THEN 1 ELSE 0 END) AS null_money,
+	SUM(CASE WHEN coffee_name IS NULL OR TRIM(coffee_name) = '' THEN 1 ELSE 0 END) AS null_coffee_name
+from coffee_sales;
