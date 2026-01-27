462a6dce9af3aebe8c1d01efb75297dbf670b542
Rryanpaak <park784612@gmail.com>
2026-01-27 15:52:51 +0900

    Update 03_product_analysis.sql

--- sql/03_product_analysis.sql
+++ sql/03_product_analysis.sql
@@ -1,1 +1,13 @@
 -- Product analysis and performance queries
+
+-- Product KPI included the top-selling products, the top-revenue products, and the top-aov.
+SELECT
+	coffee_name,
+	sum(money) as total_revenue,
+	count(*) as total_orders,
+	round(sum(money) / count(*),2) as aov
+from `coffee_sales`
+group by coffee_name
+order by total_revenue desc;
+
+
