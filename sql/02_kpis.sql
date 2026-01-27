6e5bbd889274a09bd5d331c06ea0e03be8d59c26
Rryanpaak <park784612@gmail.com>
2026-01-27 14:37:28 +0900

    Update 02_kpis.sql

--- sql/02_kpis.sql
+++ sql/02_kpis.sql
@@ -5,5 +5,46 @@ select
 	sum(money) as total_revenue,
 	count(*) as total_orders,
 	round(sum(money) * 10 / count(*),2) as aov
+from `coffee_sales`;
+
+-- Monthly KPI
+select
+	Month_name,
+	sum(money) as total_revenue,
+	count(*) as total_orders,
+	round(sum(money) * 10 / count(*),2) as aov
 from `coffee_sales`
+group by Month_name
+order by total_revenue asc;
+
+--Weekday KPI
+select
+	Weekday,
+	sum(money) as total_revenue,
+	count(*) as total_orders,
+	round(sum(money) * 10 / count(*),2) as aov
+from `coffee_sales`
+group by Weekday
+order by total_revenue asc;
+
+--Time of day KPI
+select
+	Time_of_Day,
+	sum(money) as total_revenue,
+	count(*) as total_orders,
+	round(sum(money) * 10 / count(*),2) as aov
+from `coffee_sales`
+group by Time_of_Day
+order by total_revenue asc
+	
+--Hour of day KPI
+select
+	hour_of_day,
+	sum(money) as total_revenue,
+	count(*) as total_orders,
+	round(sum(money) * 10 / count(*),2) as aov
+from `coffee_sales`
+group by hour_of_Day
+order by total_revenue asc
+
 
