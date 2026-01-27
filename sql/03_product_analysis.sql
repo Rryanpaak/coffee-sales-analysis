576972b368e9a83c4fc56b455deb842e6857b0c7
Rryanpaak <park784612@gmail.com>
2026-01-27 16:39:50 +0900

    Update 03_product_analysis.sql

--- sql/03_product_analysis.sql
+++ sql/03_product_analysis.sql
@@ -10,4 +10,31 @@ from `coffee_sales`
 group by coffee_name
 order by total_revenue desc;
 
+--Using Pareto Analysis, we can find out where it get 80% of revenue.
+WITH product_revenue as(
+	SELECT
+		coffee_name,
+		sum(money) as revenue
+	from `coffee_sales`
+	group by coffee_name
+),
+ranked as(
+	SELECT
+		coffee_name,
+		revenue,
+		sum(revenue) over(order by revenue desc) as cumulative_revenue,
+		sum(revenue) over() as total_revenue
+	from `product_revenue`
+)
+SELECT
+	coffee_name,
+	revenue,
+	round(cumulative_revenue * 100 / total_revenue,2) as cumulative_rev_pct
+from `ranked`
+order by revenue desc;
+-- Pareto Summary
+-- Top 5 products account for 83% of total revenue.
+
+
+
 
