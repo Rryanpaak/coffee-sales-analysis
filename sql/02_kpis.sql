bb5595dd871b178087bee43f4cb990f06b49f59d
Rryanpaak <park784612@gmail.com>
2026-01-27 14:27:28 +0900

    Update 02_kpis.sql

--- sql/02_kpis.sql
+++ sql/02_kpis.sql
@@ -1,1 +1,9 @@
 -- KPI calculations for coffee sales analysis
+
+-- Whole KPI (Revenue/Orders/AOV)
+select
+	sum(money) as total_revenue,
+	count(*) as total_orders,
+	round(sum(money) * 10 / count(*),2) as aov
+from `coffee_sales`
+
