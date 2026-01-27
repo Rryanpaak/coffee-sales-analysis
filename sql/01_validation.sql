84a35c96464c81c058b876147909061e1c3f1fee
Rryanpaak <park784612@gmail.com>
2026-01-27 14:11:50 +0900

    Update 01_validation.sql

--- sql/01_validation.sql
+++ sql/01_validation.sql
@@ -17,3 +17,44 @@ SELECT
 	SUM(CASE WHEN money IS NULL OR TRIM(money) = '' THEN 1 ELSE 0 END) AS null_money,
 	SUM(CASE WHEN coffee_name IS NULL OR TRIM(coffee_name) = '' THEN 1 ELSE 0 END) AS null_coffee_name
 from coffee_sales;
+
+-- 4) Money sanity check
+SELECT
+	sum(CASE WHEN money <= 0 then 1 else 0 end) as non_positive_money,
+	min(money) as min_money,
+	max(money) as max_money,
+	avg(money) as average_money
+FROM coffee_sales;
+
+-- 5) Duplicates
+--- 1) By coffee name
+SELECT
+	coffee_name,
+	count(*) as orders
+FROM coffee_sales
+group by coffee_name
+order by orders desc;
+
+---2) By Weekday
+SELECT
+	Weekday,
+	count(*) as orders
+FROM coffee_sales
+group by Weekday
+order by orders desc;
+
+---3) By Time of day
+SELECT
+	Time_of_Day,
+	count(*) as orders
+FROM coffee_sales
+group by Time_of_Day
+order by orders desc;
+
+---4) By Month name
+SELECT
+	Month_name,
+	count(*) as orders
+FROM coffee_sales
+group by Month_name
+order by orders desc;
